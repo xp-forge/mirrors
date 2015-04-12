c695bbdfff2125e1c7330daabe3bb542588b3566
Timm Friebe <thekid@thekid.de>
2015-04-12 13:59:33 +0200

    Implement equals()

--- src/main/php/lang/mirrors/Annotation.class.php
+++ src/main/php/lang/mirrors/Annotation.class.php
@@ -1,5 +1,7 @@
 <?php namespace lang\mirrors;
 
+use util\Objects;
+
 class Annotation extends \lang\Object {
   private $type, $name, $value;
 
@@ -14,4 +16,17 @@ class Annotation extends \lang\Object {
 
   /** @return var */
   public function value() { return $this->value ? $this->value->resolve($this->type->unit()) : null; }
+
+  /**
+   * Returns whether a given value is equal to this annotation
+   *
+   * @param  var $cmp
+   * @return bool
+   */
+  public function equals($cmp) {
+    return $cmp instanceof self && (
+      $this->name === $cmp->name &&
+      Objects::equal($this->value, $cmp->value)
+    );
+  }
 }
