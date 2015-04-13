03eaf0903963f87cd6d00391ddbdb068ffb8c3cd
Timm Friebe <thekid@thekid.de>
2015-04-14 00:10:35 +0200

    Cache declaration in member variable

--- src/main/php/lang/mirrors/FromCode.class.php
+++ src/main/php/lang/mirrors/FromCode.class.php
@@ -4,27 +4,28 @@ use lang\mirrors\parse\ClassSyntax;
 use lang\mirrors\parse\ClassSource;
 
 class FromCode extends \lang\Object implements Source {
-  private $unit;
+  private $unit, $decl;
 
   public function __construct($name) {
     $this->unit= (new ClassSyntax())->parse(new ClassSource(strtr($name, '\\', '.')));
+    $this->decl= $this->unit->declaration();
   }
 
   /** @return string */
   public function typeName() { 
     $package= $this->unit->package();
-    return ($package ? $package.'.' : '').$this->unit->declaration()['name'];
+    return ($package ? $package.'.' : '').$this->decl['name'];
   }
 
   /** @return string */
-  public function typeDeclaration() { return $this->unit->declaration()['name']; }
+  public function typeDeclaration() { return $this->decl['name']; }
 
   /** @return string */
   public function packageName() { return $this->unit->package(); }
 
   /** @return string */
   public function typeParent() {
-    $parent= $this->unit->declaration()['parent'];
+    $parent= $this->decl['parent'];
     return $parent ? new self($this->resolve($parent)) : null;
   }
 
