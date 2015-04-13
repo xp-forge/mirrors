c8afe2f1cf5b227409c44e9c65bc1cee0ffc593e
Timm Friebe <thekid@thekid.de>
2015-04-13 22:07:03 +0200

    Add various values for backing

--- src/test/php/lang/mirrors/unittest/ValueTest.class.php
+++ src/test/php/lang/mirrors/unittest/ValueTest.class.php
@@ -5,7 +5,17 @@ use lang\mirrors\parse\Value;
 class ValueTest extends ResolveableTest {
 
   #[@test]
-  public function resolve_returns_backing_value() {
-    $this->assertEquals(1, (new Value(1))->resolve($this->type));
+  public function resolve_returns_backing_null() {
+    $this->assertNull((new Value(null))->resolve($this->type));
+  }
+
+  #[@test, @values([
+  #  [0], [1], [-1], [1.5], [-1.5],
+  #  [true], [false],
+  #  [''], ['Test'],
+  #  [[]], [[1, 2, 3]], [['key' => 'value']]
+  #])]
+  public function resolve_returns_backing_value($value) {
+    $this->assertEquals($value, (new Value($value))->resolve($this->type));
   }
 }
