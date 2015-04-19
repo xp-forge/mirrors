0d21cd910d633413e96aed4eac2d59725bb40fb3
Timm Friebe <thekid@thekid.de>
2015-04-20 00:22:14 +0200

    QA: Add class api documentation

--- src/main/php/lang/mirrors/Sources.class.php
+++ src/main/php/lang/mirrors/Sources.class.php
@@ -1,5 +1,15 @@
 <?php namespace lang\mirrors;
 
+/**
+ * Sources from which reflection can be created:
+ *
+ * - DEFAULT: Uses reflection if class exists, parsing code otherwise
+ * - REFLECTION: Uses ext/reflection
+ * - CODE: Parses code.
+ *
+ * Has special case handling to cope with situation that class is not
+ * fully defined (e.g. when performing compile-time metaprogramming).
+ */
 abstract class Sources extends \lang\Enum {
   public static $DEFAULT, $REFLECTION, $CODE;
 
@@ -10,16 +20,16 @@ abstract class Sources extends \lang\Enum {
       public function reflect($class, $source= null) {
         if ($class instanceof \ReflectionClass) {
           return new FromReflection($class, $source ?: $this);
+        }
+
+        $literal= strtr($class, ".", "\\\\");
+        $dotted= strtr($class, "\\\\", ".");
+        if (class_exists($literal) || interface_exists($literal) || trait_exists($literal)) {
+          return self::$REFLECTION->reflect($class, $source ?: $this);
+        } else if (\lang\ClassLoader::getDefault()->providesClass($dotted)) {
+          return new FromCode($dotted, $source ?: $this);
         } else {
-          $literal= strtr($class, ".", "\\\\");
-          $dotted= strtr($class, "\\\\", ".");
-          if (class_exists($literal) || interface_exists($literal) || trait_exists($literal)) {
-            return self::$REFLECTION->reflect($class, $source ?: $this);
-          } else if (\lang\ClassLoader::getDefault()->providesClass($dotted)) {
-            return new FromCode($dotted, $source ?: $this);
-          } else {
-            return new FromIncomplete($literal);
-          }
+          return new FromIncomplete($literal);
         }
       }
     }');
@@ -27,11 +37,12 @@ abstract class Sources extends \lang\Enum {
       static function __static() { }
 
       public function reflect($class, $source= null) {
+        if ($class instanceof \ReflectionClass) {
+          return new FromReflection($class, $source);
+        }
+
         try {
-          return new FromReflection(
-            $class instanceof \ReflectionClass ? $class : new \ReflectionClass(strtr($class, ".", "\\\\")),
-            $source
-          );
+          return new FromReflection(new \ReflectionClass(strtr($class, ".", "\\\\")), $source);
         } catch (\Exception $e) {
           throw new \lang\ClassNotFoundException($class.": ".$e->getMessage());
         }
