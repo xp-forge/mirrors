c7a41aacc98333e828b0947829a7de1696ff54a6
Timm Friebe <thekid@thekid.de>
2015-04-26 17:56:56 +0200

    QA: API documentation [skip ci]

--- src/test/hack/lang/mirrors/unittest/fixture/FixtureHHVMAnnotations.class.php
+++ src/test/hack/lang/mirrors/unittest/fixture/FixtureHHVMAnnotations.class.php
@@ -1,10 +1,16 @@
 <?hh namespace lang\mirrors\unittest\fixture;
 
+/**
+ * Fixture for HHVM annotations test
+ *
+ * *Note: Fields cannot have attributes in Hack language*
+ *
+ * @see    http://docs.hhvm.com/manual/en/hack.attributes.php
+ * @see    https://github.com/facebook/hhvm/issues/3605
+ */
 <<test, runtime('~3.6'), expect(['class' => 'lang.IllegalArgumentException'])>>
 class FixtureHHVMAnnotations extends \lang\Object {
 
-  // Fields cannot have attributes in Hack language
-  // See https://github.com/facebook/hhvm/issues/3605
   #[@test, @runtime('~3.6'), @expect(class = 'lang.IllegalArgumentException')]
   public $field;
 
