d21bad59efca5ebb0d5a3a431472efd1445a6b9f
Timm Friebe <thekid@thekid.de>
2015-07-26 18:04:25 +0200

    Verify parameter and return types for ReflectionClass::hasMethod() on HHVM

--- src/test/php/lang/mirrors/unittest/NativeClassSupport.class.php
+++ src/test/php/lang/mirrors/unittest/NativeClassSupport.class.php
@@ -1,6 +1,7 @@
 <?php namespace lang\mirrors\unittest;
 
 use lang\mirrors\TypeMirror;
+use lang\Primitive;
 
 class NativeClassSupport extends \unittest\TestCase {
 
@@ -17,5 +18,14 @@ class NativeClassSupport extends \unittest\TestCase {
   #[@test]
   public function ReflectionClass_has_name_field() {
     $this->assertTrue((new TypeMirror('ReflectionClass'))->fields()->provides('name'));
+  }
+
+  #[@test, @action(new OnlyOnHHVM())]
+  public function ReflectionClass_hasMethod() {
+    $hasMethod= (new TypeMirror('ReflectionClass'))->methods()->named('hasMethod');
+    $this->assertEquals(
+      [Primitive::$STRING, Primitive::$BOOL],
+      [$hasMethod->parameters()->first()->type(), $hasMethod->returns()]
+    );
   }
 }
