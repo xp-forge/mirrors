2fa622fcf9d6703489a00d39c2102726e88474bc
Timm Friebe <thekid@thekid.de>
2015-08-25 01:04:01 +0200

    Rewrite func_get_args() to work

--- src/test/php/lang/mirrors/unittest/parse/NewInstanceTest.class.php
+++ src/test/php/lang/mirrors/unittest/parse/NewInstanceTest.class.php
@@ -15,10 +15,10 @@ class NewInstanceTest extends ResolveableTest {
 
   #[@test]
   public function passes_args_to_constructor() {
-    $fixture= newinstance('lang.Object', [], [
-      'passed'      => null,
-      '__construct' => function() { $this->passed= func_get_args(); }
-    ]);
+    $fixture= newinstance('lang.Object', [], '{
+      public $passed= null;
+      public function __construct() { $this->passed= func_get_args(); }
+    }');
     $this->assertEquals(
       ['Test', 1],
       (new NewInstance($fixture->getClassName(), [new Value('Test'), new Value(1)]))->resolve($this->source)->passed
