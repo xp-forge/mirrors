9ff4c612a7709bb2a3cd330a1a540d65b1c6cecf
Timm Friebe <thekid@thekid.de>
2015-10-10 13:56:40 +0200

    Remove duplicate tests verifiying lang.mirrors.parse.** class functionality

--- src/test/php/lang/mirrors/unittest/AnnotationTest.class.php
+++ src/test/php/lang/mirrors/unittest/AnnotationTest.class.php
@@ -2,16 +2,10 @@
 
 use lang\mirrors\Annotation;
 use lang\mirrors\TypeMirror;
-use lang\mirrors\unittest\fixture\Identity;
 use lang\mirrors\parse\Value;
-use lang\mirrors\parse\Member;
-use lang\mirrors\parse\NewInstance;
 
 class AnnotationTest extends \unittest\TestCase {
   private $type;
-
-  public static $FIXTURE = 'static';
-  const FIXTURE = 'constant';
 
   /** @return void */
   public function setUp() {
@@ -31,29 +25,6 @@ class AnnotationTest extends \unittest\TestCase {
   #[@test]
   public function value() {
     $this->assertEquals('Test', (new Annotation($this->type, 'name', new Value('Test')))->value());
-  }
-
-  #[@test]
-  public function class_reference() {
-    $this->assertEquals(self::class, (new Annotation($this->type, 'name', new Member('self', 'class')))->value());
-  }
-
-  #[@test]
-  public function class_constant() {
-    $this->assertEquals(self::FIXTURE, (new Annotation($this->type, 'name', new Member('self', 'FIXTURE')))->value());
-  }
-
-  #[@test]
-  public function static_class_member() {
-    $this->assertEquals(self::$FIXTURE, (new Annotation($this->type, 'name', new Member('self', '$FIXTURE')))->value());
-  }
-
-  #[@test]
-  public function newinstance() {
-    $this->assertEquals(
-      new Identity('Test'),
-      (new Annotation($this->type, 'name', new NewInstance('Identity', [new Value('Test')])))->value()
-    );
   }
 
   #[@test]
