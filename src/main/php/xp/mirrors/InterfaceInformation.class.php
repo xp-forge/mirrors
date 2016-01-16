dcc468f6d9a34b9a7395812151e6d19462ff1435
Timm Friebe <thekid@thekid.de>
2016-01-16 18:48:07 +0100

    Show interface parents

--- src/main/php/xp/mirrors/InterfaceInformation.class.php
+++ src/main/php/xp/mirrors/InterfaceInformation.class.php
@@ -10,7 +10,17 @@ class InterfaceInformation extends TypeKindInformation {
    */
   public function display($out) {
     $separator= false;
-    $out->write(self::declarationOf($this->mirror), ' {');
+    $out->write(self::declarationOf($this->mirror));
+
+    $extends= [];
+    foreach ($this->mirror->interfaces()->declared() as $parent) {
+      $extends[]= $parent->name();
+    }
+    if ($extends) {
+      $out->write(' extends ', implode(', ', $extends));
+    }
+
+    $out->writeLine(' {');
     $this->displayMembers($this->mirror->constants(), $out, $separator);
     $this->displayMembers($this->mirror->methods(), $out, $separator);
     $out->writeLine('}');
