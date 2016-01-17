3ff986e445ce67013c389ec1306ce3036759302b
Timm Friebe <thekid@thekid.de>
2016-01-17 12:16:54 +0100

    Add tests for classes without parents

---
+++ src/test/php/lang/mirrors/unittest/NoParentTest.class.php
@@ -0,0 +1,39 @@
+<?php namespace lang\mirrors\unittest;
+
+use lang\mirrors\TypeMirror;
+use lang\IllegalStateException;
+
+abstract class NoParentTest extends \unittest\TestCase {
+  use TypeDefinition;
+
+  #[@test, @expect(IllegalStateException::class)]
+  public function return_type() {
+    $this->mirror('{ /** @return parent */ public function fixture() { } }', [])
+      ->methods()
+      ->named('fixture')
+      ->returns()
+    ;
+  }
+
+  #[@test, @expect(IllegalStateException::class)]
+  public function parameter_type() {
+    $this->mirror('{ /** @param parent */ public function fixture($param) { } }', [])
+      ->methods()
+      ->named('fixture')
+      ->parameters()
+      ->first()
+      ->type()
+    ;
+  }
+
+  #[@test, @expect(IllegalStateException::class)]
+  public function annotation_value() {
+    $this->mirror("{ #[@fixture(new parent())]\npublic function fixture() { } }", [])
+      ->methods()
+      ->named('fixture')
+      ->annotations()
+      ->named('fixture')
+      ->value()
+    ;
+  }
+}
